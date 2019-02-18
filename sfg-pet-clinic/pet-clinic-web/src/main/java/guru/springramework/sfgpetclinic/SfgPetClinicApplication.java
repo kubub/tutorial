77e27cc41f5c4f1a1e6f8eb82ac2061ad7ed7d51
Jakub Mocny <kububm@gmail.com>
2019-02-18 09:21:41 +0100

    Index page added

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springramework/sfgpetclinic/SfgPetClinicApplication.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springramework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,7 +1,11 @@
 package guru.springramework.sfgpetclinic;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SfgPetClinicApplication {
 	public static void main(String[] args) {
-
+		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
 }
